a75886a3f4affcdd0f349848ee3b6a9127b50658
ryan_pku <778315181@qq.com>
2020-01-13 10:07:16 +0800

    provide new model DeepLab and NCRF trainset

--- basic/models/scannet.py
+++ basic/models/scannet.py
@@ -8,7 +8,7 @@ import sys
 class Scannet(nn.Module):
     def __init__(self, in_channels=3, out_channels=2, pretrained=True):
         super(Scannet, self).__init__()
-        vgg =models.vgg16(pretrained,model_dir="/userhome/renqian/download_models")
+        vgg =models.vgg16(pretrained)
         features = list(vgg.features.children())
 
         # Set padding=0 in features
@@ -21,6 +21,23 @@ class Scannet(nn.Module):
             nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=1, stride=1, padding=0),
             nn.Conv2d(in_channels=1024, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
         )
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        '''
+        _initialize_weights for new conv2
+        '''
+        for m in self.classifier():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         x = self.features(x)
